c9e0cdcd9fa9ad5fcf4ec053569ed85c6180f4b3
rslwnyx <aleynanuraslan1997@gmail.com>
2025-11-26 22:53:44 +0300

    upgrading syntax and adding comments

--- src/config.py
+++ src/config.py
@@ -1,4 +1,13 @@
-vocab_weight = 0.4
-grammar_weight = 0.6
-hsk_data_path = r"data\hsk_data.csv"
-hsk_composition_path = r"data\hsk_composition.csv"
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+#Scoring weights
+VOCAB_WEIGHT = 0.4
+GRAMMAR_WEIGHT = 0.5
+SENTENCE_LENGTH_WEIGHT = 0.1
+
+#Data file paths
+HSK_DATA_PATH = r"data\hsk_data.csv"
+HSK_COMP_PATH= r"data\hsk_composition.csv"
+
