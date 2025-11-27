6e9755629852de63978f7cf75d267ee758dc8cd1
rslwnyx <aleynanuraslan1997@gmail.com>
2025-11-27 19:06:49 +0300

    adjusting the clean_text function

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,6 +1,9 @@
 import jieba
 import pandas as pd
 import re
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class Preprocessor:
     def __init__(self, user_dict_path=None):
@@ -11,6 +14,7 @@ class Preprocessor:
     def clean_text(text):
         clean_text = re.sub(r'\{.*?\}|\[.*?\]|[a-zA-Z0-9]', '', text)
         clean_text = re.sub(r'\s+', '', clean_text)
+        clean_text = clean_text.replace('.+', '').replace('\\', '').strip()
         return clean_text
 
     @staticmethod
