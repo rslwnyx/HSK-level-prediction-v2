544069046017ea755ea3fb862d73fa261795742e
rslwnyx <aleynanuraslan1997@gmail.com>
2025-11-27 22:07:40 +0300

    Small changes

--- src/config.py
+++ src/config.py
@@ -7,6 +7,11 @@ VOCAB_WEIGHT = 0.4
 GRAMMAR_WEIGHT = 0.5
 SENTENCE_LENGTH_WEIGHT = 0.1
 
+#Penalties and bonuses
+PENALTY_THRESHOLD = 3.0
+PENALTY_RATE = 0.2
+LENGHTS_BONUS_THRESHOLD = 15
+
 #Data file paths
 HSK_DATA_PATH = r"data\hsk_data.csv"
 HSK_COMP_PATH= r"data\hsk_composition.csv"
